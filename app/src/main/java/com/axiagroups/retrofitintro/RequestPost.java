e3914cc1074211ee0826fd8a9dd6b1f49d5194e0
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-04 12:22:57 +0600

    retrofit get post done

---
+++ app/src/main/java/com/axiagroups/retrofitintro/RequestPost.java
@@ -0,0 +1,26 @@
+package com.axiagroups.retrofitintro;
+
+public class RequestPost {
+    String name, job;
+
+    public RequestPost(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
